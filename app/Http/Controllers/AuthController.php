bed3bdd75ab809d52e953bcbe24735af7091c038
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-22 11:08:52 +0800

    jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,44 +5,111 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use Laravel\Passport\HasApiTokens;
 use Validator;
 
 class AuthController extends Controller
 {
-  public function login(Request $request) {
-    $validator = Validator::make($request->all(), [
-      'email' => 'required|email',
-      'password' => 'required|string|min:6',
-    ]);
-    if ($validator->fails()) {
-      $data = ['error' => $validator->errors()->toJson(), 'success' => false];
-      return response()->json($data, 400);
+  /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
-    $token = auth('api')->attempt($validator->validated());
-    dd($token);
-    if (!auth('web')->attempt($validator->validated())) {
-      return response(['error_message' => 'Incorrect Credentials. Please try again']);
-    }
-    $token = auth()->user()->createToken('API Token')->accessToken;
-    return response(['user' => auth()->user(), 'token' => $token]);
-  }
 
-  public function register(Request $request)
-  {
-    $validator = Validator::make($request->all(), [
-      'name' => 'required|string|between:2,100',
-      'email' => 'required|string|email|max:100|unique:users',
-      'password' => 'required|string|confirmed|min:6',
-    ]);
-    if($validator->fails()){
-      $data = ['error' => $validator->errors()->toJson(), 'success' => false];
-      return response()->json($data, 400);
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request){
+    	$validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        if (! $token = auth()->attempt($validator->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
     }
-    $data = $request->only('name', 'email', 'password');
-    $data['password'] = bcrypt($request->password);
-    $user = User::create($data);
-    $token = $user->createToken('API Token')->accessToken;
-    return response([ 'user' => $user, 'token' => $token]);
-  }
+
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create(array_merge(
+                    $validator->validated(),
+                    ['password' => bcrypt($request->password)]
+                ));
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user
+        ], 201);
+    }
+
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout() {
+        auth()->logout();
+
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function userProfile() {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
 }
